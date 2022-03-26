4934afa2b82d6912154eaef7e32f6d4661730106
Keli Booster <kbooster17@gmail.com>
2022-03-26 14:17:49 +0000

    serve json weather data through route

--- src/app.js
+++ src/app.js
@@ -36,6 +36,50 @@ app.get("/help", (req, res) => {
   });
 });
 
+app.get("/weather", (req, res) => {
+  const location = req.query.search;
+  if (!location) {
+    return res.send({
+      error: "Please provide a search term!",
+    });
+  } else {
+    const getGeocode = require("./utilities/getLocation");
+    const forecast = require("./utilities/forecast");
+
+    getGeocode({ location }, (geoErr, geoData) => {
+      if (geoErr) {
+        return res.send({
+          error: geoErr,
+        });
+      } else {
+        forecast(geoData.address, (forErr, forData) => {
+          if (forErr) {
+            return res.send({
+              error: forErr,
+            });
+          }
+          console.log("data weather", forData);
+          const { main, description } = forData.weather[0];
+          res.send({
+            location: geoData.address,
+            weather: {
+              main: main,
+              description: description,
+            },
+          });
+        });
+      }
+    });
+  }
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "Not Found",
+    year: currentYear,
+  });
+});
+
 app.listen(3000, () => {
   console.log("Listening to port 3000");
 });
